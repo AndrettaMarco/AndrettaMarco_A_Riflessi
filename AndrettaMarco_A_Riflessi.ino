81ac00492a651ef920abb1da03522047a41c890f
AndrettaMarco <marco.andretta@barsanti.edu.it>
2019-03-16 11:51:31 +0100

    2.0

--- AndrettaMarco_A_Riflessi.ino
+++ AndrettaMarco_A_Riflessi.ino
@@ -1,31 +1,59 @@
-int pulsante = 6;
-int buzzer = 7;
-int led = 8;
+#include <LiquidCrystal.h>
+
+#define led 12
+#define buzzer  11
+#define pulsante 10
+
 int numrandom;
+int numrandom2;
+int tempo1;
+int tempo2=0;
+int schiacciato=0;
+LiquidCrystal lcd(2, 3, 4, 5, 6, 7);
+
 void setup() {
   // put your setup code here, to run once:
-pinMode (pulsante, OUTPUT);
-
-Serial.begin(9600);
+pinMode (pulsante, INPUT);
+pinMode (led, OUTPUT);
+lcd.begin(16, 2);
+lcd.setCursor(0, 0);
+lcd.print("Prima riga");
+//lcd.setCursor(0, 1);
+//lcd.print("seconda riga");
 randomSeed(analogRead(0));
+RiflessiLed();
+RiflessiSuono();
+
+}
+
+
+void RiflessiLed()
+{
+numrandom= random(2000, 8000);
+delay(numrandom);
+digitalWrite(led, HIGH);
+while(digitalRead(pulsante)==LOW);
+digitalWrite(led, LOW);
+}
+
+void RiflessiSuono()
+{
+numrandom2 = random (2000, 8000);
+delay(numrandom2);
+digitalWrite(buzzer, HIGH);
+tone (7, 400, 6000); //7 è la porta buzzer; suona alla frequenza di 400Hz per 6 secondi
+tempo1=millis();
+while(schiacciato==0){
+schiacciato=digitalRead(pulsante);
+if(schiacciato==HIGH){
+tempo2=millis();
+noTone(buzzer);
+}
+}
+
+schiacciato=0;
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
-Serial.println("Accensione led");
-while(Serial.available() == 0){}
-numrandom= random(2000, 10000);
-Serial.readString();
-Serial.println(numrandom);
-
-//Accensione
-delay(numrandom);
-digitalWrite(led, HIGH);
-if (pulsante == HIGH)
-{
-  pulsante == LOW;
-}
-
-setup();
 }
